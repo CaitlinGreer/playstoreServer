e30663a1246f24e018a63f3016055a379cef44e4
Caitlin Greer <clento1121@gmail.com>
2021-03-05 11:56:24 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('dev'))
+
+const playStore = require('./playstore.js')
+
+app.get('/apps', (req, res) => {
+    const { genres = "", sort} = req.query
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be either a rating or app')
+        }
+    }
+    
+    let results = playStore 
+        .filter(playApp =>
+            playApp
+                .Genres
+                .toLowerCase()
+                .includes(genres.toLowerCase()))
+    
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0
+        })
+    }
+
+    res
+        .json(results)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on Port 8000')
+})
+
+
